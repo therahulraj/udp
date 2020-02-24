e68616987a1add8296ddf26b98359f62b01c22ec
therahulraj <rahulraj16mar@gmail.com>
2020-02-25 00:42:42 +0530

    ...

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const net = require("net")
 
-// let port = process.env.PORT || 8080;
+let port = process.env.PORT || 8080;
 // var address = server.address();
 const server = net.createServer(socket => {
     socket.write("hello")
@@ -12,22 +12,33 @@ const server = net.createServer(socket => {
     }, 1000)
 })
 
-// server.listen(8080)
-server.listen(function(){
-    var address = server.address();
-    var port = address.port;
-    var family = address.family;
-    var ipaddr = address.address;
-    console.log('Server is listening at port' + port);
-    console.log('Server ip :' + ipaddr);
-    console.log('Server is IP4/IP6 : ' + family);
-  });
+server.listen(port)
+// server.listen(function(){
+//     var address = server.address();
+//     var port = address.port;
+//     var family = address.family;
+//     var ipaddr = address.address;
+//     console.log('Server is listening at port' + port);
+//     console.log('Server ip :' + ipaddr);
+//     console.log('Server is IP4/IP6 : ' + family);
+//   });
 
 // const dgram = require("dgram")
 // const socket = dgram.createSocket('udp4')
+// const message = Buffer.from('Some bytes');
 
 // socket.on('message', (msg, rinfo) => {
-//     console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`)
+//     console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+//     ;
 // })
 
-// socket.bind(8081);
+// // socket.send(message, 8081, 'localhost', (err) => {
+// //     client.close();
+// //   });
+
+// // socket.on('listening', () => {
+// //     const address = socket.address();
+// //     console.log(`server listening ${address.address}:${address.port}`);
+// //   });
+
+// // socket.bind(8081);
