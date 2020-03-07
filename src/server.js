2bb5f020894f8c7940b39c6f51fb8af45d2b25b6
therahulraj <rahulraj16mar@gmail.com>
2020-03-07 13:41:46 +0530

    ....

--- src/server.js
+++ src/server.js
@@ -3,107 +3,114 @@ const net = require("net");
 const http = require('http');
 
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
 const socketIO= require('socket.io');
 
-let global_values = {
-    color: 'white',
-    range: '1'
-}
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
 
 
 
-//***********************configuring TCP server*************************************************************************
+let all_devices_value = ["0,0", "0,0", "0,0", "0,0"];
+
+
+
+//***************************configuring TCP server***************************************************************
 const TCP_port = 8080; //TCP port no.
 const TCP_server = net.createServer() //TCP server created
 
 
-//***********************configuring HTTP server***********************************************************************
+//****************************configuring HTTP server**************************************************************
 const HTTP_port = 80; //HTTP port no.
 const app = express();
 const HTTP_server = http.createServer(app);
 const io = socketIO(HTTP_server);
 
 
-//****************************serving the static web pages*************************************************************
+//****************************serving the static web pages*********************************************************
 const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath))
-global_HTTP_socket = undefined;
-global_TCP_socket = undefined;
 
-function check_HTTP_connection () {
-    io.on('connection', (HTTP_socket) => {
-        global_HTTP_socket = HTTP_socket;
-        io.emit('currentValues', global_values);
-        HTTP_socket.on('updateColor', (color) => {
-            global_values.color = color;
-            if (global_TCP_socket) {
-                global_TCP_socket.write(`${global_values.color} ${global_values.range}`)
+
+function update_check_save_value(data) {
+    single_device_value = data.split(",");
+    let single_device_index = parseInt(single_device_value[0]);
+    let single_device_state = parseInt(single_device_value[1]);
+    let single_device_speed = parseInt(single_device_value[2]);
+    if (single_device_value.length == 3) {
+        if (single_device_index >= 0 && single_device_index <= all_devices_value.length) {
+            if (single_device_state == 0 || single_device_state == 1) {
+                if (single_device_speed >= 0 && single_device_speed <= 9) {
+                    all_devices_value[single_device_index] = `${single_device_state},${single_device_speed}`;
+                    console.log(single_device_value);
+                    return true;
+                }
             }
-            console.log(global_values);
-            io.emit('currentValues', global_values);
-        })
-        HTTP_socket.on('updateRange', (range) => {
-            global_values.range = range;
-            console.log(global_values);
-            if (global_TCP_socket) {
-                global_TCP_socket.write(`${global_values.color} ${global_values.range}`)
-            }
-            io.emit('currentValues', global_values);
-        })
-    })
+        }
+    }
+    return false;
 }
 
 
+//*****************************for HTTP web socket****************************************************************
+io.on('connection', (HTTP_socket) => {
 
+    HTTP_socket.emit('setupValue', all_devices_value.join(";"));
 
+    emitter.on('valueChangedByTCP', (data) => {
+        HTTP_socket.emit('updatedValue', data);
+    })
 
-
-function check_TCP_connection () {
-    console.log('under TcP connection.')
-    TCP_server.on('connection', handleConnection);
-
-    function handleConnection(TCP_socket) {
-        // console.log(socket)
-        // console.log(socket.remotePort)
-        // console.log(socket.clientIMEInumber)
-        global_TCP_socket = TCP_socket;
-        // TCP_socket.write("rahul");
-        TCP_socket.on('data', onData)
-        TCP_socket.on('close', onClose)
-        TCP_socket.on('error', onError)
-
-        function onData(data) {
-            res_data = data.toString().trim();
-            if (parseInt(res_data)) {
-                global_values.range = parseInt(res_data).toString();
-            } else {
-                if (res_data == 'green' || res_data == 'white') {
-                    global_values.color = res_data;
-                }
-            }
-        global_TCP_socket.write(`${global_values.color} ${global_values.range}`)
-        if (global_HTTP_socket) {
-            io.emit('currentValues', global_values)
+    HTTP_socket.on('updateValue', (data) => {
+        if (update_check_save_value(data)) {
+            emitter.emit('valueChangedByHTTP', data);
+            io.emit('updatedValue', data);
         }
-        TCP_socket.write
+    })
+})
+
+
+
+
+// *************************************for TCP socket *********************************************************
+
+TCP_server.on('connection', handleConnection);
+
+function handleConnection(TCP_socket) {
+
+    TCP_socket.write(all_devices_value.join(";"));
+
+    emitter.on('valueChangedByHTTP', (data) => {
+        TCP_socket.write(data);
+    })
+
+    emitter.on('valueChangedByTCP', (data) => {
+        TCP_socket.write(data);
+    })
+        
+    TCP_socket.on('data', onData)
+    TCP_socket.on('close', onClose)
+    TCP_socket.on('error', onError)
+
+    function onData(data) {
+        data = data.toString().trim();
+        if (update_check_save_value(data)) {
+            emitter.emit('valueChangedByTCP', data);
         }
-        function onClose() {
-            console.log("one connection closed.")
-        }
-        function onError(error) {
-            console.log(error)
-        }
+    }
+
+    function onClose() {
+        console.log("one connection closed.")
+    }
+
+    function onError(error) {
+         console.log(error)
     }
 }
 
-    
 
 
 
-check_HTTP_connection();
-check_TCP_connection();
+
 
 
 
@@ -112,6 +119,8 @@ check_TCP_connection();
 TCP_server.listen(TCP_port, () => {
     console.log(`TCP server is up on ${TCP_port}`)
 })
+
+
 HTTP_server.listen(HTTP_port, () => {
     console.log(`HTTP server is up on ${HTTP_port}`)
 })
